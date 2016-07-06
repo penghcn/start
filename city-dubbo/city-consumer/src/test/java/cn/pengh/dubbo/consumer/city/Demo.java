162ae95e55ef20c3f7b7d39b8d38b8b547d3d33c
p_fuiou <qingseyunlan@gmail.com>
2016-07-06 14:12:58 +0800

    some

--- city-dubbo/city-consumer/src/test/java/cn/pengh/dubbo/consumer/city/Demo.java
+++ city-dubbo/city-consumer/src/test/java/cn/pengh/dubbo/consumer/city/Demo.java
@@ -10,25 +10,18 @@ import cn.pengh.library.Log;
 import cn.pengh.mvc.core.listener.AppCtxKeeper;
 
 public class Demo {
-	private CityService cityService ;
-	public String get(int id){
-		return cityService.get(new CityRequest(36)).getNm();
-	}
-	
 	
 	public static void main(String[] args) {
-		//final String port = "8888";
-		
 		//测试常规服务
 		RegisterHelper.init();
         CityService cityService = AppCtxKeeper.getBean(CityService.class);
         
         Log.debug(RpcContext.getContext().isConsumerSide());
         
-        ClazzHelper.print(cityService.get(new CityRequest(36)));
-        ClazzHelper.print(cityService.get(new CityRequest(3607)));
+        ClazzHelper.print(cityService.get(new CityRequest(36)));//360000 江西省
+        ClazzHelper.print(cityService.get(new CityRequest(3607)));//360700 江西省赣州市
         ClazzHelper.print(cityService.get(new CityRequest(360782)));
-        ClazzHelper.print(cityService.get(new CityRequest(310115)));
+        ClazzHelper.print(cityService.get(new CityRequest(310115)));//310115 上海市上海市浦东新区
         
         Log.debug(RpcContext.getContext().isConsumerSide());
 	}
