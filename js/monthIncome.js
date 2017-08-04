57b126e13d0ca37dfca3634e25c5a14b86267a8b
penghcn <qingseyunlan@gmail.com>
2017-08-04 16:08:10 +0800

    Create monthIncome.js

---
+++ js/monthIncome.js
@@ -0,0 +1,9 @@
+//等额本息每月还款计算
+//本金，总期数(月)，年利率
+function monthIncome(invest, totalMonth, yearRate) {
+    var monthRate = yearRate/totalMonth;
+    var monthIncome = invest * monthRate * Math.pow(1 + monthRate, totalMonth) / (Math.pow(1 + monthRate, totalMonth) - 1);
+    console.log(monthIncome);
+    return monthIncome.toFixed(4);
+}
+monthIncome(52000,12,0.083);
