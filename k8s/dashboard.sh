ec706eb4af0d0fcfdc806d843949e263217e3d7c
pengh-mbp <qingseyunlan@gmail.com>
2021-03-23 17:05:01 +0800

    opt

---
+++ k8s/dashboard.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# curl -sSL http://192.168.8.251/open/doc/raw/master/k8s/dashboard.sh | sh
+
+
+# 管理员账户名称
+username=admin-user
+node_port=30043
+
+kubectl delete ns kubernetes-dashboard 
+
+# https://blog.csdn.net/networken/article/details/85607593
+# https://github.com/kubernetes/dashboard
+curl https://raw.githubusercontent.com/kubernetes/dashboard/v2.2.0/aio/deploy/recommended.yaml > recommended.yaml
+
+#修改yaml配置超时时间12h
+sed -i '/--namespace=kubernetes-dashboard/a\ \ \ \ \ \ \ \ \ \ \ \ - --token-ttl=43200' recommended.yaml
+
+kubectl apply -f recommended.yaml
+
+cat > dashboard-adminuser.yaml << EOF
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: $username
+  namespace: kubernetes-dashboard
+
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: $username
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+- kind: ServiceAccount
+  name: $username
+  namespace: kubernetes-dashboard  
+
+EOF
+
+kubectl apply -f dashboard-adminuser.yaml
+
+# 改成nodePort 暴露
+kubectl  patch svc kubernetes-dashboard -n kubernetes-dashboard \
+-p '{"spec":{"type":"NodePort","ports":[{"port":443,"targetPort":8443,"nodePort":$node_port}]}}'
+
+
+#查看admin-user账户的token
+# kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
+
+kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep $username | awk '{print $1}')
+
+
+local_ip=$(echo $(ip addr | grep bond0 | awk '/^[0-9]+: / {}; /inet.*global/ {print gensub(/(.*)\/(.*)/, "\\1", "g", $2)}')| awk '{print $1}')
+
+# 使用firefox 访问
+# curl -ki https://192.168.8.122:30443
+echo "请使用firefox访问如下地址，登录token参考上文"
+echo "https://$local_ip:$node_port"
+
+
